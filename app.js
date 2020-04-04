138ecf5e5e1ef0636091fa77ef9d01e3ee81e0a4
Sean <seanom96@gmail.com>
2020-04-04 23:34:47 +0100

    view engine finally works

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const router = require('./routes')
-app.use(express.static('public'))
+app.set('views','views')
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use('/',router)
-
 app.listen(3000,()=>{
     console.log('listening')
 })
