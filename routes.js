0d0a6f9b5d8cc40f79a6e3aa1c44a382d34cbda6
Sean <seanom96@gmail.com>
2020-04-06 16:01:40 +0100

    basic routes work

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const router = express.Router()
-router.get('/',(req, res)=>{
-    res.render('index')
-})
-module.exports = router
+const express = require('express');
+const router = express.Router();
+router.get('/', (req, res) => {res.render('index')});
+router.get('/about', (req, res) => {res.render('about')});
+router.get('/contact', (req, res) => {res.render('contact')});
+router.get('/login', (req, res) => {res.render('login')});
+router.get('/register', (req, res) => {res.render('register')});
+router.get('/posts', (req, res) => {res.render('posts')});
+module.exports = router;
