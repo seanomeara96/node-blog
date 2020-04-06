cb1ffb16144965043f1de23f18a771588c4e86f7
Sean <seanom96@gmail.com>
2020-04-06 22:30:28 +0100

    removed body parser

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
 const router = require('./router')
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json())
-app.set('views','views')
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use('/',router)
-app.listen(3000)
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.set('views','views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('/', router);
+app.listen(3000);
