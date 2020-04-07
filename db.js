7461c1a706d06bd0f420acf55e9d836740bd07d8
Sean <seanom96@gmail.com>
2020-04-07 18:12:20 +0100

    app connects to db on start up

--- db.js
+++ db.js
@@ -1,9 +1,22 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+const server = require('./app');
+dotenv.config();
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'postgres',
+    database: 'database1',
     password: process.env.PASSWORD,
     post: 5432
 });
-module.exports = pool
+async function startApplication () {
+    await pool.connect().then((client) => {
+        console.log('Successful connection to the database');
+        server.listen(3000);
+        module.exports = pool;
+        client.release();
+    }).catch((err) => {
+        console.log('Cannot connect to database', err);
+    });
+};
+startApplication();
