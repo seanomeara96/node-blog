577b9faee2b6015de6ea47f7dc330e97ec4c914e
Sean <seanom96@gmail.com>
2020-04-08 12:22:00 +0100

    added log in/out controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,43 @@
 const User = require('../models/User');
 
 exports.logIn = (req, res) => {
-    
+    console.log("user is logging in:", req.body)
+    let user = new User(req.body)
+    user.logIn()
+    .then((response) => {
+        console.log("Log in success:", response);
+        res.redirect('/');
+    })
+    .catch(err => {
+        console.log("Error loggin in", err);
+        res.redirect('/login');
+    });
 };
 
 exports.logOut = (req, res) => {
-
+    console.log("User is logging out:", req.body);
+    let user = new User(req.body);
+    user.logOut()
+    .then(response => {
+        console.log("Log out success:", response);
+        res.redirect('/');
+    })
+    .catch(err => {
+        console.log("Error logging out:", err);
+        res.redirect('/');
+    });
 };
 
 exports.register = (req, res) => {
-    // Let the user new a new instance of User
-    console.log('User is registering', req.body);
+    console.log('User is registering:', req.body);
     let user = new User(req.body);
     user.register()
-    .then((response) => {
-        console.log(response.rows);
+    .then(response => {
+        console.log("Registration success:", response.rows);
         res.redirect('login');
-    }).catch((err) => {
-        console.log("something bad happened", err);
+    })
+    .catch(err => {
+        console.log("Error registering user:", err);
+        res.redirect('/register');
     });
 };
