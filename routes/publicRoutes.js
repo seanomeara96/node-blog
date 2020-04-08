ca3e9af649c7da9b3a91c567fb24c58466316bed
Sean <seanom96@gmail.com>
2020-04-08 12:27:56 +0100

    added 404 routes

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', (req, res) => {res.render('index')});
-router.get('/about', (req, res) => {res.render('about')});
-router.get('/contact', (req, res) => {res.render('contact')});
-router.get('/login', (req, res) => {res.render('login')});
-router.get('/register', (req, res) => {res.render('register')});
-router.get('/posts', (req, res) => {res.render('posts')});
+router.get('/', (req, res) => res.render('index'));
+router.get('/about', (req, res) => res.render('about'));
+router.get('/contact', (req, res) => res.render('contact'));
+router.get('/login', (req, res) => res.render('login'));
+router.get('/register', (req, res) => res.render('register'));
+router.get('/posts', (req, res) => res.render('posts'));
+router.get('/404', (req, res) => res.render('404'));
 module.exports = router;
