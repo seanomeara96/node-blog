41591328c4ed45033b102e5a79a71b367fc4f5cc
Sean <seanom96@gmail.com>
2020-04-08 12:07:19 +0100

    cleaned user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,8 @@ exports.register = (req, res) => {
     // Let the user new a new instance of User
     console.log('User is registering', req.body);
     let user = new User(req.body);
-    user.register().then((response) => {
+    user.register()
+    .then((response) => {
         console.log(response.rows);
         res.redirect('login');
     }).catch((err) => {
