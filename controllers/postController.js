903a8ea847de37e656f0d8f8c472bf21e7cda49a
Sean <seanom96@gmail.com>
2020-04-10 14:55:51 +0100

    updated postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -13,7 +13,7 @@ exports.create =  (req, res) => {
     .then(response => {
         // respond with post id
         let newId = response.pid;
-        res.redirect(`/posts/${newId}`);
+        res.redirect(`/posts/${ newId }`);
     }).catch(err => {
         console.log("Error creating post:", err);
         res.render("404");
@@ -21,21 +21,30 @@ exports.create =  (req, res) => {
 };
 
 exports.delete = (req, res) => {
-
+    console.log("Deleting post:")
+    Post.delete()
+    .then(() => res.redirect('/posts'))
+    .catch(err => console.log("There was an error deleting the post"));
 };
 
 exports.edit = (req, res) => {
-
+    let post = new Post(re.body)
+    try{
+        let updatedPost = post.update();
+        res.render('post-single', { post: updatedPost });
+    } catch (err) {
+        console.log("There was an error updating this post");
+    };
 };
 
 exports.viewFeed = async (req, res) => {
     try {
         let feed = await Post.viewFeed();
         console.log("viewfeed query resolved");
-        res.render('posts', {posts: feed});
+        res.render('posts', { posts: feed });
     } catch (err) {
         console.log("viewfeed query rejected", err);
-        res.render('posts', {posts: err});
+        res.render('404');
     };
 };
 
@@ -44,7 +53,7 @@ exports.viewSingle = async (req, res) => {
         // query db for post with an id == req.params.id
         let singlePost = await Post.view(req.params.id);
         // return object
-        res.render('post-single', {post: singlePost});
+        res.render('post-single', { post: singlePost });
     } catch (err) {
         console.log("Error fetching post:", err);
         res.render('404');
