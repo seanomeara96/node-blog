fee9aeb1dc6ea58b265592250e55c76ac0a674ba
Sean <seanom96@gmail.com>
2020-04-13 22:29:46 +0100

    data available to all templates

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index')
+    console.log("remember those locals you asked for?", res.locals)
+    console.log("req.session", req.session)
 });
 router.get('/about', (req, res) => res.render('about'));
 router.get('/contact', (req, res) => res.render('contact'));
